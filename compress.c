e7861928fe62e01d3e724c31800e01da716b9a17
Yukariko <rkdals7979@naver.com>
2014-12-05 15:21:14 +0900

    tab fix

--- compress.c
+++ compress.c
@@ -2,24 +2,24 @@
 
 char *get_encode_name(char *path)
 {
-  int len = strlen(path);
-  char *ret = (char *)malloc(len + 1);
-  int i;
-  strcpy(ret,path);
+	int len = strlen(path);
+	char *ret = (char *)malloc(len + 1);
+	int i;
+	strcpy(ret,path);
 	for(i=len-1;ret[i] != '.';i--);
-  strcpy(ret+i,".cpd");
-  return ret;
+	strcpy(ret+i,".cpd");
+	return ret;
 }
 
 char *get_decode_name(char *path)
 {
-  int len = strlen(path);
-  char *ret = (char *)malloc(len + 1);
-  int i;
-  strcpy(ret,path);
+	int len = strlen(path);
+	char *ret = (char *)malloc(len + 1);
+	int i;
+	strcpy(ret,path);
 	for(i=len-1;ret[i] != '.';i--);
-  strcpy(ret+i,".dcpd");
-  return ret;
+	strcpy(ret+i,".dcpd");
+	return ret;
 }
 
 int get_size(FILE *fp)
@@ -32,7 +32,7 @@ int get_size(FILE *fp)
 }
 void get_buffer(FILE *fp,char *buffer)
 {
-	for(;*(buffer++)=fgetc(fp),!feof(fp););
+	for(;*(buffer++)=fgetc(fp),!feof(fp);
 }
 
 int get_name(char *path)
@@ -44,28 +44,28 @@ int get_name(char *path)
 
 void set_buffer(FILE *fp,char *buffer, int size)
 {
-  fwrite(&size,1,sizeof(int),fp);
-  for(;size--;)fputc(*(buffer++),fp);	
+	fwrite(&size,1,sizeof(int),fp);
+	for(;size--;)fputc(*(buffer++),fp);	
 }
 void compress(char *path, int option)
 {
 	FILE *fp;
-  char *buffer, *result;
-  int size,rsize,i;
-  
-  // get buffer from file
-  fp = fopen(path,"rb");
-  if(fp == 0){puts("[!] fopen error");return;}
-  size = get_size(fp);
-  buffer = (char *)malloc(size+0xFFFFFF);
-  get_buffer(fp,buffer);
-  fclose(fp);
-  
-  int name = get_name(path);
-  
-  result = (char *)malloc(size+0xFFFFFF);
-  // encode
-  if (option == 0)
+	char *buffer, *result;
+	int size,rsize,i;
+	
+	// get buffer from file
+	fp = fopen(path,"rb");
+	if(fp == 0){puts("[!] fopen error");return;}
+	size = get_size(fp);
+	buffer = (char *)malloc(size+0xFFFFFF);
+	get_buffer(fp,buffer);
+	fclose(fp);
+	
+	int name = get_name(path);
+	
+	result = (char *)malloc(size+0xFFFFFF);
+	// encode
+	if (option == 0)
 	{
 		rsize = encode(buffer,size,result,name);
 	}
@@ -75,11 +75,11 @@ void compress(char *path, int option)
 	{
 		
 	}
-  // set buffer to file
-  printf("%d to %d\n",size,rsize);
-  fp = fopen(get_encode_name(path),"wb");
-  set_buffer(fp,result,rsize);
-  fclose(fp);  
+	// set buffer to file
+	printf("%d to %d\n",size,rsize);
+	fp = fopen(get_encode_name(path),"wb");
+	set_buffer(fp,result,rsize);
+	fclose(fp);  
 }
 
 int encode(char *buffer, int size, char *result, int name)
